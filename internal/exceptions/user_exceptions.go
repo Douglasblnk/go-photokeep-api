aabc7dbe3d144a7198770c7c8c33cfe2075d90ec
Douglas Blank <douglasblank1@gmail.com>
2022-02-24 21:56:19 -0300

    feat(user): implemented dynamodb getItem

---
+++ internal/exceptions/user_exceptions.go
@@ -0,0 +1,5 @@
+package exceptions
+
+import "errors"
+
+var ErrUserNotFound = errors.New("user not found")
--- internal/repository/user/find_user_by_id.go
+++ internal/repository/user/find_user_by_id.go
@@ -2,25 +2,46 @@ package userRepository
 
 import (
 	"go-dynamodb/internal/database"
+	"go-dynamodb/internal/exceptions"
 	"go-dynamodb/internal/models"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 )
 
 type Item struct {
-	Year  int
-	Title string
+	ID   int
+	Name string
 }
 
 func FindUserByID(id string) (*models.User, error) {
-	// user := &models.User{}
+	user := &models.User{}
 
 	db := dynamodb.New(database.Session)
 
-	db.GetItem(&dynamodb.GetItemInput{
+	params := &dynamodb.GetItemInput{
 		TableName: aws.String("Auth.User"),
-	})
+		Key: map[string]*dynamodb.AttributeValue{
+			"key": {
+				S: aws.String(id),
+			},
+		},
+	}
 
-	return nil, nil
+	result, err := db.GetItem(params)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if result.Item == nil {
+		return nil, exceptions.ErrUserNotFound
+	}
+
+	if err = dynamodbattribute.UnmarshalMap(result.Item, user); err != nil {
+		return nil, err
+	}
+
+	return user, nil
 }
